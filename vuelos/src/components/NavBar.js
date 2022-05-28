d83a06b2abdb15e27ae9ad452dbc17edc77ab76b
patricioherrera2022 <herrerapatricio281@gmail.com>
2022-05-28 17:59:42 -0300

    main

---
+++ vuelos/src/components/NavBar.js
@@ -0,0 +1,16 @@
+const NavBar = ()  => {
+    return (
+          <nav>
+              <button>destinos</button>
+              <button>adicionales</button>
+              <button>En Avion</button>
+              <button>En Bus</button>
+          </nav>
+  
+  
+    )
+    
+  
+  }
+  
+  export default NavBar
